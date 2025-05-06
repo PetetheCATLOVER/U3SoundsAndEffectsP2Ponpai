1198832dc4edcadade5db22dc168a989ee774208
SeanJ07 <91712813+SeanJ07@users.noreply.github.com>
2025-05-06 09:56:07 -0700

    U3SoundsAndEffectsP2Ponpai
    i need to lock in

--- U3SoundsAndEffectsPonpaiP2/Assets/SScripts/MoveLeft.cs
+++ U3SoundsAndEffectsPonpaiP2/Assets/SScripts/MoveLeft.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class MoveLeft : MonoBehaviour
 {
+    private float speed = 30;
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.Translate(Vector3.left * Time.deltaTime * speed);
     }
 
     // Update is called once per frame
